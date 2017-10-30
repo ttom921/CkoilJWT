59fbfe1707917ef6d42ab9518c897fbef90cd46b
ttom921 <ttom921@hotmail.com>
2017-10-30 16:25:59 +0800

    這是第一版webapi

---
+++ WebApi/Providers/CustomAuthProvider.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Owin.Security.OAuth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using System.Security.Claims;
+
+namespace WebApi.Providers
+{
+    //基本的Access Token發放。
+    public class CustomAuthProvider: OAuthAuthorizationServerProvider
+    {
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            context.Validated();
+        }
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            // 建立identity，用於API中授權方式 EX:[Authorize(Roles = "Administrators, Member")]
+            var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+            if(context.UserName=="admin" && context.Password== "admin")
+            {
+                //playload
+                identity.AddClaim(new Claim(ClaimTypes.Role, "admin"));
+                identity.AddClaim(new Claim("username", "admin"));
+                identity.AddClaim(new Claim(ClaimTypes.Name, "Sourav Mondal"));
+                context.Validated(identity);
+            }else if ( context.UserName=="user" && context.Password=="user")
+            {
+                //playload
+                identity.AddClaim(new Claim(ClaimTypes.Role, "user"));
+                identity.AddClaim(new Claim("username", "user"));
+                identity.AddClaim(new Claim(ClaimTypes.Name, "tom tang湯"));
+                context.Validated(identity);
+            }
+            else
+            {
+                context.SetError("invalid_grant", "Provided username and password is incorrect");
+                return;
+            }
+
+        }
+    }
+}
---
+++ WebApi/Startup.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using Microsoft.Owin.Security.OAuth;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(WebApi.Startup))]
+
+namespace WebApi
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+            //enable cors origin requests
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+            //初始token provider
+            ConfigureOAuth(app);
+            HttpConfiguration config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+        }
+
+        private void ConfigureOAuth(IAppBuilder app)
+        {
+            var CustomAuthProvider = new Providers.CustomAuthProvider();
+            OAuthAuthorizationServerOptions options = new OAuthAuthorizationServerOptions()
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromMinutes(30), //TimeSpan.FromDays(1),
+                //RefreshTokenProvider = new CustomRefreshTokenProvider()
+                Provider = CustomAuthProvider
+            };
+            // Token Generation
+            app.UseOAuthAuthorizationServer(options);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+        }
+    }
+}
