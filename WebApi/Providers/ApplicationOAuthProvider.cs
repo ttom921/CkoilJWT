7c95931045c064b838fb44eca2d728f61f62a8f7
ttom921 <ttom921@hotmail.com>
2017-11-02 17:09:50 +0800

    修改檔名

--- WebApi/Providers/ApplicationOAuthProvider.cs
+++ WebApi/Providers/ApplicationOAuthProvider.cs
@@ -9,22 +9,34 @@ using System.Security.Claims;
 namespace WebApi.Providers
 {
     //基本的Access Token發放。
-    public class CustomAuthProvider: OAuthAuthorizationServerProvider
+    public class ApplicationOAuthProvider: OAuthAuthorizationServerProvider
     {
         public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
         {
-            context.Validated();
+            string clientId = string.Empty;
+            string clientSecret = string.Empty;
+            //Client client = null;
+
+            // Resource owner password credentials does not provide a client ID.
+            if (context.ClientId == null)
+            {
+                context.Validated();
+            }
         }
         public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
         {
             // 建立identity，用於API中授權方式 EX:[Authorize(Roles = "Administrators, Member")]
+            // 驗證方式
             var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+
             if(context.UserName=="admin" && context.Password== "admin")
             {
                 //playload
                 identity.AddClaim(new Claim(ClaimTypes.Role, "admin"));
                 identity.AddClaim(new Claim("username", "admin"));
                 identity.AddClaim(new Claim(ClaimTypes.Name, "Sourav Mondal"));
+                identity.AddClaim(new Claim("Point", "5000"));
+               
                 context.Validated(identity);
             }else if ( context.UserName=="user" && context.Password=="user")
             {
@@ -32,6 +44,8 @@ namespace WebApi.Providers
                 identity.AddClaim(new Claim(ClaimTypes.Role, "user"));
                 identity.AddClaim(new Claim("username", "user"));
                 identity.AddClaim(new Claim(ClaimTypes.Name, "tom tang湯"));
+                identity.AddClaim(new Claim("Point", "10"));
+               
                 context.Validated(identity);
             }
             else
---
+++ WebApi/Providers/SimpleRefreshTokenProvider.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Infrastructure;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+
+namespace WebApi.Providers
+{
+    public class SimpleRefreshTokenProvider: IAuthenticationTokenProvider
+    {
+        //實際放到DB去存起來
+        private static ConcurrentDictionary<string, AuthenticationTicket> _refreshTokens =
+            new ConcurrentDictionary<string, AuthenticationTicket>();
+
+       
+
+        public async Task CreateAsync(AuthenticationTokenCreateContext context)
+        {
+            var guid = Guid.NewGuid().ToString();
+            //copy properties and set the desired lifetime of refresh token
+            var refreshTokenProperties = new AuthenticationProperties(context.Ticket.Properties.Dictionary)
+            {
+                IssuedUtc = context.Ticket.Properties.IssuedUtc,
+                 //時間是3個月
+                ExpiresUtc = DateTime.UtcNow.AddMonths(1)
+            };
+            var refreshTokenTicket = new AuthenticationTicket(context.Ticket.Identity, refreshTokenProperties);
+            _refreshTokens.TryAdd(guid, refreshTokenTicket);
+            context.SetToken(guid);
+        }
+
+
+
+        public async Task ReceiveAsync(AuthenticationTokenReceiveContext context)
+        {
+            AuthenticationTicket ticket;
+            if (_refreshTokens.TryRemove(context.Token, out ticket))
+            {
+                context.SetTicket(ticket);
+            }
+        }
+
+        public void Create(AuthenticationTokenCreateContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Receive(AuthenticationTokenReceiveContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
